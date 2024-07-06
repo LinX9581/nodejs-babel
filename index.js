55d6bbb53d2c3b028362891d64975755baa7ead7
root <root@stg-devops.asia-east1-b.c.nownews-news-stg.internal>
2024-07-06 15:26:54 +0800

    add schedule

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import bodyParser from 'body-parser';
 import cors from 'cors';
 import 'dotenv/config';
 import './global'
+import './schedule/schedule'
 import indexRouter from './route/indexRouter';
 const app = express();
 const http = require('http').Server(app);
@@ -18,7 +19,7 @@ app.use('/', indexRouter);
 console.log(process.env.test);
 
 const host = '0.0.0.0';
-const port = process.env.PORT || 3006;
+const port = process.env.PORT || 3011;
 
 http.listen(port, host, function() {
     console.log("Server started on " + port);
